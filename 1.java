5bb0ba6c3c66e5b8aa7eaf6834a683462ebd6b76
hgfejcwoefGleb <gipilipenko@edu.hse.ru>
2022-11-24 18:07:51 +0300

    2

--- 1.java
+++ 1.java
@@ -2,9 +2,11 @@ import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        label1: System.out.println("Введите день своего рождения: ");
-        while(!sc.hasNextInt()){
-            if (sc.hasNextInt())
+        System.out.println("Введите день своего рождения: ");
+        int bigday = sc;
+        while(big day<=1||>=31)
+                {
+        if (bigday<=31&&>=1)
                 System.out.println();
             else {
                 System.out.println("Ошибка, введите число!");
