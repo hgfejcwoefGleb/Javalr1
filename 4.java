9d341623093af56c0a5d2b0366e84f1af68a86d0
hgfejcwoefGleb <gipilipenko@edu.hse.ru>
2022-11-25 04:21:47 +0300

    100501

--- 4.java
+++ 4.java
@@ -8,35 +8,35 @@ public class Main{
             System.out.println("Введите,пожалуйста, целое или дробное число через запятую: ");
             sc.next();
         }
-        double x = sc.nextDouble();
+        double digit = sc.nextDouble();
         System.out.println("Введите степень, в которую хотите возвести X: ");
         while (!sc.hasNextDouble())
         {
             System.out.println("Введите,пожалуйста, целое или дробное число через запятую: ");
             sc.next();
         }
-        double n = sc.nextDouble();
-        boolean rangeN = (n >= -15 && n<=15);
-        while (rangeN == false) {
+        double stepen = sc.nextDouble();
+        boolean rangeStepen = (stepen >= -15 && stepen <= 15);
+        while (rangeStepen == false) {
             System.out.println("Введите, пожалуста, степень от -15 до 15: ");
             while (!sc.hasNextDouble()) {
                 System.out.println("Введите, пожалуста, степень от -15 до 15: ");
                 sc.next();
             }
-            n = sc.nextDouble();
-            if (n >= -15 && n<=15)
-                rangeN = true;
+            stepen = sc.nextDouble();
+            if (stepen >= -15 && stepen <= 15)
+                rangeStepen = true;
         }
         double result = 1;
-        if( n > 0 )
-            for (int i = 1; i <=n; i++){
-                result = x*result;
+        if( stepen > 0 )
+            for (int i = 1; i <=stepen; i++){
+                result = digit*result;
             }
-        if(n == 0)
+        if(stepen == 0)
             result = 1;
-        if( n < 0)
-            for (int u = -1; u >= n; u--){
-                result = result/x;
+        if( stepen < 0)
+            for (int u = -1; u >= stepen; u--){
+                result = result/digit;
             }
         System.out.println("Исходный результат равен: " + result);
     }}
